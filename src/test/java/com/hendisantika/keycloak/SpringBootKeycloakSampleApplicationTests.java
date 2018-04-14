b06dc11f527c457b6f11fa3c2a7f4efd54e494f7
Hendi Santika <hendisantika@gmail.com>
2018-04-14 21:10:14 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/keycloak/SpringBootKeycloakSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.keycloak;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootKeycloakSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
